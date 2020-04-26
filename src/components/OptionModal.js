e1c388c1c45d7497cd3dfc12e168011f0beca67c
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2020-04-26 15:58:22 +0100

    style modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,14 @@ const OptionModal = ({ selectedOption, handleClearSelectedOption }) => (
     isOpen={!!selectedOption}
     onRequestClose={handleClearSelectedOption}
     contentLabel="Selected Option"
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {selectedOption && <p>{selectedOption}</p>}
-    <button onClick={handleClearSelectedOption}>Okay</button>
+    <h3 className="modal__title">You Should</h3>
+    {selectedOption && <p className="modal__body">{selectedOption}</p>}
+    <button className="button" onClick={handleClearSelectedOption}>
+      Okay
+    </button>
   </Modal>
 );
 
